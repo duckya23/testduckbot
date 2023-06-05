4f3de5a695992efee130bcb61d712ed2f5586935
chyyyy15 <131245872+chyyyy15@users.noreply.github.com>
2023-06-05 16:19:45 +0800

    Update cheat.py

--- cheat.py
+++ cheat.py
@@ -1,3 +1,19 @@
 app = Flask(__name__)
 line_bot_api = LineBotApi('cA4Y+naWER+/PyaAYGacJOO6GznxzUz/vUHhFzmY2eVvIczqzh7InjUL9h2IPo50NOyMXbbZXrNUYySlyd2CXHtlKYTDlkhIdGitB/ZKTvULCJnDf4PoVKyHgZRp7FIuHZcLErYwgQ2/Xd1ZRiFr4QdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('05a15941e79753db37de1062d1cec8dd')
+
+
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
