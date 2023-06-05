d053fa2ca7312d345e72cadde0ca949ce257024b
chyyyy15 <131245872+chyyyy15@users.noreply.github.com>
2023-06-05 16:15:50 +0800

    Create cheat.py

---
+++ cheat.py
@@ -0,0 +1,3 @@
+app = Flask(__name__)
+line_bot_api = LineBotApi('cA4Y+naWER+/PyaAYGacJOO6GznxzUz/vUHhFzmY2eVvIczqzh7InjUL9h2IPo50NOyMXbbZXrNUYySlyd2CXHtlKYTDlkhIdGitB/ZKTvULCJnDf4PoVKyHgZRp7FIuHZcLErYwgQ2/Xd1ZRiFr4QdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('05a15941e79753db37de1062d1cec8dd')
